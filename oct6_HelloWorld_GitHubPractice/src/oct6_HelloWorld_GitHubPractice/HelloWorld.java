a4c21434d57ff0933f2fd18e8ebdab4c921cc096
Mila Schnitzius <cschnitzius348@gmail.com>
2023-10-06 14:54:45 -0700

    removed bobo from greeting

--- oct6_HelloWorld_GitHubPractice/src/oct6_HelloWorld_GitHubPractice/HelloWorld.java
+++ oct6_HelloWorld_GitHubPractice/src/oct6_HelloWorld_GitHubPractice/HelloWorld.java
@@ -3,7 +3,7 @@ package oct6_HelloWorld_GitHubPractice;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Bobo");
+		System.out.println("Hello World");
 	}
 
 }
